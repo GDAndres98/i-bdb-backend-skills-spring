a156e5ea4060add24e3216f04057552f9c49617b
Andres Osorio <gd_andres98@hotmail.com>
2022-04-10 04:32:42 -0500

    Product Repository and Service

---
+++ src/main/java/bdb/progskills/service/ProductService.java
@@ -0,0 +1,40 @@
+package bdb.progskills.service;
+
+import bdb.progskills.model.Product;
+import bdb.progskills.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+
+@AllArgsConstructor
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getProductsByIds(List<Long> productIds) {
+        HashSet<Long> uniqueProductIds = new HashSet<Long>(productIds);
+        List<Product> products = productRepository.findByIdIn(new ArrayList<Long>(uniqueProductIds));
+        if (uniqueProductIds.size() != products.size())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Some products were not found");
+        return products;
+    }
+
+
+}
