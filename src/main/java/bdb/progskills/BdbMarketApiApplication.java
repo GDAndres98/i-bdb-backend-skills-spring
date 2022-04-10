edbd3d41bc3b56f91b58cf65ad1966a735c040f5
Andres Osorio <gd_andres98@hotmail.com>
2022-04-09 20:37:04 -0500

    Initial Commit

---
+++ src/main/java/bdb/progskills/BdbMarketApiApplication.java
@@ -0,0 +1,13 @@
+package bdb.progskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdbMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdbMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bdb/progskills/BdbMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bdb.progskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdbMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
